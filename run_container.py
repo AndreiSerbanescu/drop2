1fafdd1fe85ddaf99b61326e13f00a5bca69c4b7
Andrei Serbanescu <andrei.serbanescu16@imperial.ac.uk>
2020-04-02 11:45:59 +0300

    by default perform only registration, add --with_mask_segmentation to get segmentation as well

--- run_container.py
+++ run_container.py
@@ -11,6 +11,8 @@ if __name__ == "__main__":
     parser.add_argument('-o', '--output', required=True, type=str, help="Host path to output directory")
     parser.add_argument('-s', '--source', default="", type=str, help="(Optional) Host path to source volume directory")
     parser.add_argument('-m', '--mask', default="", type=str, help="(Optional) Host path to source mask directory")
+    parser.add_argument('--with-mask-segmentation', action='store_true', help="Select this to only perform registration")
+    parser.add_argument('--debug', action='store_true', help="Select this for container to not perform any computation")
 
     args = parser.parse_args()
 
@@ -32,6 +34,16 @@ if __name__ == "__main__":
             volumes.append('{}:/home/mask'.format(args.mask))
 
         dc['services']['drop2']['volumes'] = volumes
+
+        if args.debug:
+            dc['services']['drop2']['command'] = 'tail -F anything'
+
+        environment = ["DEBUG={}".format(args.debug), "WITH_MASK_SEGMENTATION={}".format(args.with_mask_segmentation)]
+
+        old_env = dc['services']['drop2'].get('environment', [])
+        dc['services']['drop2']['environment'] = environment + old_env
+
+        print(dc['services']['drop2']['environment'])
 
     new_dc_file = 'docker-compose-tmp.yml'
 
--- start.py
+++ start.py
@@ -1,38 +1,51 @@
 import subprocess as sb
+import os
 
-reg_cmd = "/home/build/drop/apps/dropreg/dropreg -s /home/source/ " \
-          "-t /home/target/ " \
-          "-o /home/interm/interm.nii.gz --ocompose -c -l --lsim 1 --ltype 2 --llevels 8 8 8 4 4 4 " \
-          "--lsampling 0.1 -n --nsim 1 --nffd 80 --nlevels 8 8 8 4 4 4 4 4 4 --nlambda 0.5"
+def run_registration():
+    reg_cmd = "/home/build/drop/apps/dropreg/dropreg -s /home/source/ " \
+              "-t /home/target/ " \
+              "-o /home/interm/interm.nii.gz --ocompose -c -l --lsim 1 --ltype 2 --llevels 8 8 8 4 4 4 " \
+              "--lsampling 0.1 -n --nsim 1 --nffd 80 --nlevels 8 8 8 4 4 4 4 4 4 --nlambda 0.5"
 
-seg_heart_cmd = "/home/build/drop/apps/dropreg/dropreg " \
-          "-s /home/masks/mask_Heart.nii.gz  " \
-          "-t /home/interm/interm.nii.gz " \
-          "-o /home/output/mask_heart.nii.gz " \
-          "--ointerp 0 " \
-          "--fx /home/interm/interm_field_x.nii.gz " \
-          "--fy /home/interm/interm_field_y.nii.gz " \
-          "--fz /home/interm/interm_field_z.nii.gz"
+    sb.call([reg_cmd], shell=True)
 
-seg_lung_l_cmd = "/home/build/drop/apps/dropreg/dropreg " \
-          "-s /home/masks/mask_Lung_L.nii.gz  " \
-          "-t /home/interm/interm.nii.gz " \
-          "-o /home/output/mask_lung_l.nii.gz " \
-          "--ointerp 0 " \
-          "--fx /home/interm/interm_field_x.nii.gz " \
-          "--fy /home/interm/interm_field_y.nii.gz " \
-          "--fz /home/interm/interm_field_z.nii.gz"
 
-seg_lung_r_cmd = "/home/build/drop/apps/dropreg/dropreg " \
-          "-s /home/masks/mask_Lung_R.nii.gz  " \
-          "-t /home/interm/interm.nii.gz " \
-          "-o /home/output/mask_lung_r.nii.gz " \
-          "--ointerp 0 " \
-          "--fx /home/interm/interm_field_x.nii.gz " \
-          "--fy /home/interm/interm_field_y.nii.gz " \
-          "--fz /home/interm/interm_field_z.nii.gz"
+def run_mask_segmentations():
 
-sb.call([reg_cmd], shell=True)
-sb.call([seg_heart_cmd], shell=True)
-sb.call([seg_lung_l_cmd], shell=True)
-sb.call([seg_lung_r_cmd], shell=True)
+    seg_heart_cmd = "/home/build/drop/apps/dropreg/dropreg " \
+              "-s /home/masks/mask_Heart.nii.gz  " \
+              "-t /home/interm/interm.nii.gz " \
+              "-o /home/output/mask_heart.nii.gz " \
+              "--ointerp 0 " \
+              "--fx /home/interm/interm_field_x.nii.gz " \
+              "--fy /home/interm/interm_field_y.nii.gz " \
+              "--fz /home/interm/interm_field_z.nii.gz"
+
+    seg_lung_l_cmd = "/home/build/drop/apps/dropreg/dropreg " \
+              "-s /home/masks/mask_Lung_L.nii.gz  " \
+              "-t /home/interm/interm.nii.gz " \
+              "-o /home/output/mask_lung_l.nii.gz " \
+              "--ointerp 0 " \
+              "--fx /home/interm/interm_field_x.nii.gz " \
+              "--fy /home/interm/interm_field_y.nii.gz " \
+              "--fz /home/interm/interm_field_z.nii.gz"
+
+    seg_lung_r_cmd = "/home/build/drop/apps/dropreg/dropreg " \
+              "-s /home/masks/mask_Lung_R.nii.gz  " \
+              "-t /home/interm/interm.nii.gz " \
+              "-o /home/output/mask_lung_r.nii.gz " \
+              "--ointerp 0 " \
+              "--fx /home/interm/interm_field_x.nii.gz " \
+              "--fy /home/interm/interm_field_y.nii.gz " \
+              "--fz /home/interm/interm_field_z.nii.gz"
+
+    sb.call([seg_heart_cmd], shell=True)
+    sb.call([seg_lung_l_cmd], shell=True)
+    sb.call([seg_lung_r_cmd], shell=True)
+
+
+run_registration()
+
+with_mask_segmentation = os.environ.get('WITH_MASK_SEGMENTATION', False)
+if with_mask_segmentation:
+    run_mask_segmentations()
